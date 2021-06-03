bfef7d4dd66c9d4374b04515bd23ce7b00f34be2
Marcin Nahajowski <immortalmar0@gmail.com>
2021-06-03 18:17:19 +0200

    Add admin model sso

--- api/src/main.py
+++ api/src/main.py
@@ -6,8 +6,12 @@ from map import get_map
 from data import get_stations, get_station_data
 from validator import validate_params
 from fastapi.middleware.cors import CORSMiddleware
+from user_model import *
+from admin_usage import *
 
 app = FastAPI()
+
+
 origins = [
     "http://localhost:8080"
 ]
@@ -18,6 +22,36 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.post("/admin_page")
+async def admin_page(config: Config, current_user: User = Depends(get_current_user)):
+    if current_user.accLvl != 3:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Unathorized access",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    else:
+        change_config(config)
+        return {'success': True}
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
 
 @app.get("/map/")
 async def map_root(rect: str, width: int = 800, param: str = 'pm25', date: str = 'latest', segments_x: int = 8):
